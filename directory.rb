aadbae288340a9c98d24f0cf759d7d6f35b66e46
liskowsky <karollebiedzinski@gmail.com>
2016-06-22 17:21:13 +0100

    Directory.rb update 7 (user input)

--- directory.rb
+++ directory.rb
@@ -1,17 +1,5 @@
 student_count = 11
 
-students = [{name: "Dr. Hannibal Lecter", cohort: :november},
-            {name: "Darth Vader", cohort: :november},
-            {name: "Nurse Ratched", cohort: :november},
-            {name: "Michael Corleone", cohort: :november},
-            {name: "Alex DeLarge", cohort: :november},
-            {name: "The Wicked Witch of the West", cohort: :november},
-            {name: "Terminator", cohort: :november},
-            {name: "Freddy Krueger", cohort: :november},
-            {name: "The Joker", cohort: :november},
-            {name: "Joffrey Baratheon", cohort: :november},
-            {name: "Norman Bates", cohort: :november}]
-
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
@@ -27,6 +15,21 @@ def print_footer(students)
   print "Overall, we have #{students.count} great students."
 end
 
+def input_students
+  puts "Please enter the names of new students"
+  puts "To finish, just press ENTER twice"
+  students = [] #new empty array of students
+  name = gets.chomp
+
+  while !name.empty? do #while we got an input do the following:
+    students << {name: name, cohort: :november} #adds a new hash with an input name
+    puts "Now we have #{students.count} students" #informs how many students we currently have
+    name = gets.chomp
+  end
+  students #returns array of students
+end
+
+students = input_students
 print_header
 print_students(students)
 print_footer(students)
