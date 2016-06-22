584f4b6d13fd9cef31c8b9c53034c4f3d1ed7684
liskowsky <karollebiedzinski@gmail.com>
2016-06-22 17:35:48 +0100

    Directory.rb update 8 (ex1)

--- directory.rb
+++ directory.rb
@@ -6,8 +6,8 @@ def print_header
 end
 
 def print_students(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each.with_index(1) do |student, index|
+    puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
